291301b0caeba56fa058ed8e0444f12f179946dd
NotYourAbu <68179601+SyntaxErrorSolos@users.noreply.github.com>
2023-06-25 21:05:35 +0500

    Main page.

---
+++ app/portfolio/[id]/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+      <div>
+        <div className="h-screen flex">
+          <div className="m-auto">
+            <h1 className="text-9xl ">
+  
+            </h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
